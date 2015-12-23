d42f8a19b3915981ed7042afc23b7f9fb1c2fb87
R1G <rigved.deshpande@gmail.com>
2015-12-23 00:00:37 -0800

    Added an attackable enemy gameObject

---
+++ Assets/Scripts/EnemyScript.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyScript : MonoBehaviour {
+
+	public int enemyHealth;
+	public int enemyDamage;
+	GameObject playerTroop;
+
+	public float distanceX;
+	public float distanceZ;
+
+	public float meleeRangeX;
+	public float meleeRangeZ;
+
+	public bool selectedUnitExists;
+
+	void Start() {
+		enemyDamage = 10;
+		enemyHealth = 10;
+	}
+
+	void Update() {
+		playerTroop = GameObject.FindGameObjectWithTag("SelectedTroop");
+
+		DefineDistances ();
+		//AttackCheck ();
+		EnemyDeath ();
+	}
+
+	void OnMouseUp() {
+		if (selectedUnitExists) {
+			enemyHealth -= enemyDamage;
+		}
+	}
+
+	void EnemyDeath() {
+		if (enemyHealth <= 0) {
+			Destroy(gameObject);
+		}
+	}
+
+	void AttackCheck() {
+		if (playerTroop != null) {
+			if (distanceX <= meleeRangeX) {
+				if (distanceZ <= meleeRangeZ) {
+					selectedUnitExists = true;
+				}
+			}
+		} else {
+			selectedUnitExists = false;
+		}
+	}
+
+	void DefineDistances() {
+		if (playerTroop != null) {
+			float distanceX = Mathf.Abs (playerTroop.transform.position.x - gameObject.transform.position.x);
+			float distanceZ = Mathf.Abs (playerTroop.transform.position.z - gameObject.transform.position.z);
+
+			if(distanceX <= 2) {
+				if(distanceZ <= 2) {
+					selectedUnitExists = true;
+				}
+			}
+		}
+	}
+}
