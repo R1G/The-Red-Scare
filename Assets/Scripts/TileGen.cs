59fc61ac4c380d3d2d5236c2a9987a03770fd3df
Walmik Deshpande <wdeshpande@paypal.com>
2015-12-24 20:02:05 -0800

    Abstracted tile generation
    * Created map for tiles
    * Moved tile generation to us map

--- Assets/Scripts/TileGen.cs
+++ Assets/Scripts/TileGen.cs
@@ -18,74 +18,74 @@ public class TileGen : MonoBehaviour {
 			for(int y = 0; y < mapSizeY; y++){
 				tiles[x,y] = 0;
 			}
-				//insert addition tile instructions here
-			tiles[0,6] = DIRT_RAMP;
-			tiles[1,6] = DIRT_RAMP;
-			tiles[2,6] = DIRT_RAMP;
-			tiles[3,6] = DIRT_RAMP;
-			tiles[4,6] = DIRT_RAMP;
-			tiles[5,6] = DIRT_RAMP;
-			tiles[6,6] = DIRT_RAMP;
-			tiles[7,6] = DIRT_RAMP;
-			tiles[8,6] = DIRT_RAMP;
-			tiles[9,6] = DIRT_RAMP;
-
-			tiles[0,7] = GRASS_SLAB;
-			tiles[1,7] = GRASS_SLAB;
-			tiles[2,7] = GRASS_SLAB;
-			tiles[3,7] = GRASS_SLAB;
-			tiles[4,7] = GRASS_SLAB;
-			tiles[5,7] = GRASS_SLAB;
-			tiles[6,7] = GRASS_SLAB;
-			tiles[7,7] = GRASS_SLAB;
-			tiles[8,7] = GRASS_SLAB;
-			tiles[9,7] = GRASS_SLAB;
-
-			tiles[0,8] = GRASS_SLAB;
-			tiles[1,8] = GRASS_SLAB;
-			tiles[2,8] = GRASS_SLAB;
-			tiles[3,8] = GRASS_SLAB;
-			tiles[4,8] = GRASS_SLAB;
-			tiles[5,8] = GRASS_SLAB;
-			tiles[6,8] = GRASS_SLAB;
-			tiles[7,8] = GRASS_SLAB;
-			tiles[8,8] = GRASS_SLAB;
-			tiles[9,8] = GRASS_SLAB;
-
-			tiles[0,9] = GRASS_SLAB;
-			tiles[1,9] = GRASS_SLAB;
-			tiles[2,9] = GRASS_SLAB;
-			tiles[3,9] = GRASS_SLAB;
-			tiles[4,9] = GRASS_SLAB;
-			tiles[5,9] = GRASS_SLAB;
-			tiles[6,9] = GRASS_SLAB;
-			tiles[7,9] = GRASS_SLAB;
-			tiles[8,9] = GRASS_SLAB;
-			tiles[9,9] = GRASS_SLAB;
-
-			tiles[0,10] = GRASS_SLAB;
-			tiles[1,10] = GRASS_SLAB;
-			tiles[2,10] = GRASS_SLAB;
-			tiles[3,10] = GRASS_SLAB;
-			tiles[4,10] = GRASS_SLAB;
-			tiles[5,10] = GRASS_SLAB;
-			tiles[6,10] = GRASS_SLAB;
-			tiles[7,10] = GRASS_SLAB;
-			tiles[8,10] = GRASS_SLAB;
-			tiles[9,10] = GRASS_SLAB;
-
-			tiles[0,11] = GRASS_SLAB;
-			tiles[1,11] = GRASS_SLAB;
-			tiles[2,11] = GRASS_SLAB;
-			tiles[3,11] = GRASS_SLAB;
-			tiles[4,11] = GRASS_SLAB;
-			tiles[5,11] = GRASS_SLAB;
-			tiles[6,11] = GRASS_SLAB;
-			tiles[7,11] = GRASS_SLAB;
-			tiles[8,11] = GRASS_SLAB;
-			tiles[9,11] = GRASS_SLAB;
-
 		}
+
+		//insert addition tile instructions here
+		tiles[0,6] = DIRT_RAMP;
+		tiles[1,6] = DIRT_RAMP;
+		tiles[2,6] = DIRT_RAMP;
+		tiles[3,6] = DIRT_RAMP;
+		tiles[4,6] = DIRT_RAMP;
+		tiles[5,6] = DIRT_RAMP;
+		tiles[6,6] = DIRT_RAMP;
+		tiles[7,6] = DIRT_RAMP;
+		tiles[8,6] = DIRT_RAMP;
+		tiles[9,6] = DIRT_RAMP;
+		
+		tiles[0,7] = GRASS_SLAB;
+		tiles[1,7] = GRASS_SLAB;
+		tiles[2,7] = GRASS_SLAB;
+		tiles[3,7] = GRASS_SLAB;
+		tiles[4,7] = GRASS_SLAB;
+		tiles[5,7] = GRASS_SLAB;
+		tiles[6,7] = GRASS_SLAB;
+		tiles[7,7] = GRASS_SLAB;
+		tiles[8,7] = GRASS_SLAB;
+		tiles[9,7] = GRASS_SLAB;
+		
+		tiles[0,8] = GRASS_SLAB;
+		tiles[1,8] = GRASS_SLAB;
+		tiles[2,8] = GRASS_SLAB;
+		tiles[3,8] = GRASS_SLAB;
+		tiles[4,8] = GRASS_SLAB;
+		tiles[5,8] = GRASS_SLAB;
+		tiles[6,8] = GRASS_SLAB;
+		tiles[7,8] = GRASS_SLAB;
+		tiles[8,8] = GRASS_SLAB;
+		tiles[9,8] = GRASS_SLAB;
+		
+		tiles[0,9] = GRASS_SLAB;
+		tiles[1,9] = GRASS_SLAB;
+		tiles[2,9] = GRASS_SLAB;
+		tiles[3,9] = GRASS_SLAB;
+		tiles[4,9] = GRASS_SLAB;
+		tiles[5,9] = GRASS_SLAB;
+		tiles[6,9] = GRASS_SLAB;
+		tiles[7,9] = GRASS_SLAB;
+		tiles[8,9] = GRASS_SLAB;
+		tiles[9,9] = GRASS_SLAB;
+		
+		tiles[0,10] = GRASS_SLAB;
+		tiles[1,10] = GRASS_SLAB;
+		tiles[2,10] = GRASS_SLAB;
+		tiles[3,10] = GRASS_SLAB;
+		tiles[4,10] = GRASS_SLAB;
+		tiles[5,10] = GRASS_SLAB;
+		tiles[6,10] = GRASS_SLAB;
+		tiles[7,10] = GRASS_SLAB;
+		tiles[8,10] = GRASS_SLAB;
+		tiles[9,10] = GRASS_SLAB;
+		
+		tiles[0,11] = GRASS_SLAB;
+		tiles[1,11] = GRASS_SLAB;
+		tiles[2,11] = GRASS_SLAB;
+		tiles[3,11] = GRASS_SLAB;
+		tiles[4,11] = GRASS_SLAB;
+		tiles[5,11] = GRASS_SLAB;
+		tiles[6,11] = GRASS_SLAB;
+		tiles[7,11] = GRASS_SLAB;
+		tiles[8,11] = GRASS_SLAB;
+		tiles[9,11] = GRASS_SLAB;
 
 		createTileVisual ();
 	}
---
+++ Assets/Scripts/TileGenerator.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TileGenerator : MonoBehaviour {
+
+	int mapCol;
+	int mapRow;
+	TileType[] tileTypes;
+
+
+	int[,] tileMap = new int[,]
+	{
+		{0, 0, 0, 0, 0, 0, 1, 5, 5, 5, 5, 5, 0, 0, 0},
+		{0, 0, 0, 0, 0, 0, 1, 5, 5, 5, 5, 5, 0, 0, 0},
+		{0, 0, 0, 0, 0, 0, 1, 5, 5, 5, 5, 5, 0, 0, 0},
+		{0, 0, 0, 0, 0, 0, 1, 5, 5, 5, 5, 5, 0, 0, 0},
+		{0, 0, 0, 0, 0, 0, 1, 5, 5, 5, 5, 5, 0, 0, 0},
+		{0, 0, 0, 0, 0, 0, 1, 5, 5, 5, 5, 5, 0, 0, 0},
+		{0, 0, 0, 0, 0, 0, 1, 5, 5, 5, 5, 5, 0, 0, 0},
+		{0, 0, 0, 0, 0, 0, 1, 5, 5, 5, 5, 5, 0, 0, 0},
+		{0, 0, 0, 0, 0, 0, 1, 5, 5, 5, 5, 5, 0, 0, 0},
+		{0, 0, 0, 0, 0, 0, 1, 5, 5, 5, 5, 5, 0, 0, 0}
+	};
+
+	// Prefab resources
+	string[] prefabResources = new string[6] {
+		"Dirt_Block",
+		"Dirt_Ramp",
+		"Dirt_Slab",
+		"Grass_Block",
+		"Grass_Ramp",
+		"Grass_Slab"
+	};
+
+	// Use this for initialization
+	void Start () {
+		int mapCol = tileMap.GetUpperBound(1);
+		int mapRow = tileMap.GetUpperBound(0);
+
+		for (int x = 0; x <= mapRow; x++) {
+			for (int z = 0; z <= mapCol; z++) {
+				int tileIndex = tileMap[x, z];
+
+				// Get already created prefab from Unity and postition/rotate it
+				Vector3 tilePos = new Vector3 (x * 2, 0, z * 2);
+				Quaternion tileRot = Quaternion.Euler(270, 0, 0);
+
+				// Instantiate a new tile with this prefab
+				GameObject tilePrefabGameObject = Instantiate(Resources.Load(prefabResources[tileIndex]), tilePos, tileRot) as GameObject;
+			}
+		}
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
