cdb6584c5394731ec030891cbbc8f1ade9ea728a
R1G <rigved.deshpande@gmail.com>
2015-12-28 15:51:08 -0800

    Added placeholder orc asset and scripts for existing tiles

---
+++ Assets/Scripts/GrassBlockTile.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GrassBlockTile : BaseTile {
+	
+	void Update() {
+		FindSelectedTroop ();
+		if (selectedTroop != null) {
+			CheckRange ();
+			if (isWithinTravelRange) {
+				SetSelectedTileColor ();
+			} else {
+				SetRegularTileColor ();
+			}
+		} else {
+			SetRegularTileColor();
+		}
+	}
+	
+	void OnMouseUp() {
+		FindSelectedTroop ();
+		CheckRange ();
+		if (isWithinTravelRange) {
+			MoveSelectedTroop ();
+		} 
+	}
+}
