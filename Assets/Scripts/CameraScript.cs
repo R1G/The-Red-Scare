43f3129bba2310ac7e28cd6acac411fbe44fbe27
R1G <rigved.deshpande@gmail.com>
2016-01-01 11:04:56 -0800

    * Fixed gitignore * Added Camera Script

---
+++ Assets/Scripts/CameraScript.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraScript : MonoBehaviour {
+
+	public float mouseSensitivity;
+
+	void Update() {
+		float horizontal = Input.GetAxis ("Horizontal") * mouseSensitivity;
+		float vertical = Input.GetAxis ("Vertical") * mouseSensitivity;
+		transform.Translate (horizontal, vertical, 0);	
+	}
+}
